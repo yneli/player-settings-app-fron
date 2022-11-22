0f539cb63ac2beeb3a522e5e72de44773ec50b6c
yneli <Mr.RadReIII@yandex.ru>
2022-11-22 14:49:42 +0300

    fetch

--- src/components/Games/Games.tsx
+++ src/components/Games/Games.tsx
@@ -3,8 +3,14 @@ import { SmallSearch } from "../SmallSearch/SmallSearch";
 import { GameItems } from "../GameItems/GameItems";
 import React from "react";
 import { Dropdown } from "../Dropdown/Dropdown";
+import { useAppDispatch } from "../../redux/hook";
+import { fetchAllGames } from "../../redux/slices/games/gamesSlice";
 
 export const Games = () => {
+    const dispatch = useAppDispatch();
+    React.useEffect(() => {
+        dispatch(fetchAllGames());
+    },[]);
     return <div className={style.games}>
         <div className={style.sorted}>
             <Dropdown/>
--- src/redux/slices/games/gamesSlice.ts
+++ src/redux/slices/games/gamesSlice.ts
@@ -1,6 +1,24 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 
+export const fetchAllGames = createAsyncThunk("games/fetchAllGames", async() => {
+    const params = {
+        "dbtype": "sel_all_games",
+    };
+    const requestOptions = {
+        method: "POST",
+        body: JSON.stringify(params),
+        headers: {
+            "Content-Type": "application/json",
+},
+          
+    };
+    const response = await fetch("https://player-info135.herokuapp.com/db", requestOptions);
+    const data = await response.json();
+    console.log(data);
+
+});
 
 const initialState = {
     items: null,
